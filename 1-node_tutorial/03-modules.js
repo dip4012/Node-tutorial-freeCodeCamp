681f3a384b2d2dc10659958fe7f8ee74ae6cf76d
Dibyendu Saha <dip4012@gmail.com>
2021-12-26 20:47:41 +0530

    ..

--- 1-node_tutorial/03-modules.js
+++ 1-node_tutorial/03-modules.js
@@ -3,10 +3,10 @@
 // CommonJS, every file is module (by default)
 // Modules - Encapsulated Code (only for share minimum)
 
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative_flavor')
-require('./7-mind_grenade')
+const names = require('./04-names')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative_flavor')
+require('./07-mind_grenade')
 
 console.log(data);
 
